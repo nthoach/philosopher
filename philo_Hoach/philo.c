dfa912629b2333f5d6faf055cb8e37d1ee8cdc61
thehoachnguyen <dr.nguyen.hoach@gmail.com>
2024-09-02 22:57:25 +0400

    mon

--- philo_Hoach/philo.c
+++ philo_Hoach/philo.c
@@ -6,14 +6,48 @@
 /*   By: nthoach <nthoach@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/29 22:35:15 by nthoach           #+#    #+#             */
-/*   Updated: 2024/09/02 22:19:40 by nthoach          ###   ########.fr       */
+/*   Updated: 2024/09/02 22:57:18 by nthoach          ###   ########.fr       */
 /*                                                                            */
 /******************************************************************************/
 
 #include "philo.h"
 
 
+int	free_param(t_state *state, pthread_mutex_t *mutex, t_param *param)
+{
+	if (state)
+		free(state);
+	if (mutex)
+		free(mutex);
+	if (param)
+		free(param);
+	return (1);
+}
+				
 
+int	agv2param(t_param *param, char **agv)
+{
+	if (check_agv(agv))
+		return (exit_err());
+	param->num_ate = 0;
+	param->sim_time = in_time();
+	param->num_philo = ft_atoi(agv[1]);
+	param->time2die = ft_atoi(agv[2]);
+	param->time2eat = ft_atoi(agv[3]);
+	param->time2sleep = ft_atoi(agv[4]);
+	if (param->num_philo <= 0 || param->time2die <=0
+		|| param->time2eat <=0 || param->time2sleep <=0)
+		return (1);
+	if (agv[5])
+	{
+		param->num_eat = ft_atoi(agv[5]);
+		if(param->num_eat <=0)
+			return (1);
+	}
+	else
+		param->num_eat = -1;
+	return (0);
+}
 
 exit_err(void)
 {
